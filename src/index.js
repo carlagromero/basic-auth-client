84e29b90a2b9c3d32d52e71993d7e9cfaf3c3173
Carla Romero <carlagromero@gmail.com>
2021-05-10 19:15:28 -0300

    chore: setup redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import reducers from './reducers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App>
-        <Route path="/" exact component={Welcome} />
-        <Route path="/signup" component={Signup} />
-      </App>
-    </Router>
+    <Provider state={createStore(reducers, {})}>
+      <Router>
+          <App>
+            <Route path="/" exact component={Welcome} />
+            <Route path="/signup" component={Signup} />
+          </App>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/auth.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  authenticated: '',
+  errorMeesage: '',
+}
+
+const authReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
