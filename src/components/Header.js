3a44a3242695d669feda4811b7b48c2cf4c96af1
Carla Romero <carlagromero@gmail.com>
2021-05-12 10:21:57 -0300

    chore: add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
-  return (
-    <div>
-      <Link to="/">Redux Auth</Link>
+  const isLogin = useSelector(state => state.auth.authenticated);
+
+  const renderLinks = () => (
+    isLogin
+    ? <div>
+      <Link to="/feature">Feature</Link>
+      <Link to="/signout">Sign Out</Link>
+    </div>
+    : <div>
       <Link to="/signup">Sign Up</Link>
       <Link to="/signin">Sign In</Link>
-      <Link to="/signout">Sign Out</Link>
-      <Link to="/feature">Feature</Link>
+    </div>
+  );
+
+  return (
+    <div className="header">
+      <Link to="/">Redux Auth</Link>
+      {renderLinks()}
     </div>
   );
 };
