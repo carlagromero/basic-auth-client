0e243cd5c8f10ced32bd8f2c5270113003c5c3c7
Carla Romero <carlagromero@gmail.com>
2021-05-11 22:25:52 -0300

    chore: update store config

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(ReduxThunk))
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers, {})}>
+    <Provider store={store}>
       <Router>
         <App>
           <Route path="/" exact component={Welcome} />
